0d8588d466b5da35f19a92831ecd5cb80985c5a4
onyivantono <ivantono.ony@gmail.com>
2022-06-05 10:51:12 +0700

    challenge 5 commit pertama

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = function sortUtama(arrnama,fnCallbackSort){
+  let arrHasil=[]; 
+  for (let a=0;a<arrnama.length;a++){
+    arrHasil.push(a+"."+arrnama[a]);
+  }
+  const arrHasils = fnCallbackSort(arrHasil);
+  return arrHasils;
+}
+
+
+// const sorter = sortUtama(names,urutkan);
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+// const sortAscending = null;
 
+const sortAscending = function urutkan(arrnya){
+  const arrsort= arrnya.sort;
+  return arrsort;
+}
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+// const sortDescending = null;
+const sortDescending = function urutkan(arrnya){
+  const arrsort= arrnya.reverse;
+  return arrsort;
+}
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
